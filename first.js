11e873484905c1b43a1416d1cf7174e31b28756a
PriyanshPanjabi0922 <priyanshpanjabi@gmail.com>
2026-04-05 15:52:50 +0530

    add project files

---
+++ first.js
@@ -0,0 +1,88 @@
+const card = document.getElementById('card');
+const input = document.getElementById('input');
+const addButton = document.getElementById('addButton');
+
+function renderFunction(presentClass,totalClass,PercentageText,RequiredText,card) {
+    const Percentage = Math.round((presentClass/totalClass)*100); 
+
+ const Requireddays = Math.max(0,Math.ceil((0.75*totalClass-presentClass)/0.25));
+
+    const Status = document.createElement('div');
+    if(Percentage>=75){
+        Status.textContent  =`Safe: Yor Attendance is higer then 75%`;
+        Status.className = "Safe";
+    } 
+    else {
+        Status.textContent = `Warning : Yor Attendance is Lower then 75%`;
+        Status.className = "Warning";
+    }
+    console.log(Status);
+
+    const PercentageText = document.createElement('div');
+    PercentageText.textContent = `Percentage : ${Percentage}`;
+
+    const RequiredText = document.createElement('div');
+    RequiredText.textContent = `Required Day : ${Requireddays}`;
+
+
+}
+
+
+
+addButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+   if(!input.value.trim()){
+    alert("Please enter Subject Name");
+    return;
+   }
+
+    const cardContainer = document.createElement('div');
+    const subjectCardContainer = document.createElement('div');
+
+    const subjectName = document.createElement('div');
+    subjectName.textContent = input.value;
+
+    subjectName.className  = "subjectName";
+
+    let presentClass = 0;
+    let totalClass = 0;
+
+    const presentButton = document.createElement('button');
+    presentButton.id = "presentButton";
+    presentButton.textContent = "Present";
+
+    const absentButton = document.createElement('button');
+    absentButton.id = "absentButton";
+    absentButton.textContent = "Absent";
+
+    const buttonContainer = document.createElement('div');
+    buttonContainer.id  ="buttonContainer";
+
+    buttonContainer.append(presentButton,absentButton);
+
+    // presentbuttons event listener
+    presentButton.addEventListener("click",(e)=>{
+        presentClass++;
+        totalClass++;
+
+        renderFunction(presentClass,totalClass,PercentageText,RequiredText,card) 
+    });
+
+    // absentButton event listener
+    absentButton.addEventListener("click",(e)=>{
+        totalClass++;
+
+        renderFunction(presentClass,totalClass,PercentageText,RequiredText,card)
+    });
+
+    
+
+    subjectCardContainer.append(subjectName,buttonContainer,PercentageText,RequiredText);
+    subjectCardContainer.id  = "subjectCardContainer";
+    cardContainer.append(subjectCardContainer);
+    cardContainer.id = "cardContainer";
+
+    card.append(cardContainer);
+
+});
