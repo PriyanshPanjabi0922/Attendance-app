27fbf5492ada4c8908be0f330aa6cf5b54fd8388
PriyanshPanjabi0922 <priyanshpanjabi@gmail.com>
2026-04-06 18:53:36 +0530

    add attendance logic with percentage and required classes calculation

--- first.js
+++ first.js
@@ -2,33 +2,6 @@ const card = document.getElementById('card');
 const input = document.getElementById('input');
 const addButton = document.getElementById('addButton');
 
-function renderFunction(presentClass,totalClass,PercentageText,RequiredText,card) {
-    const Percentage = Math.round((presentClass/totalClass)*100); 
-
- const Requireddays = Math.max(0,Math.ceil((0.75*totalClass-presentClass)/0.25));
-
-    const Status = document.createElement('div');
-    if(Percentage>=75){
-        Status.textContent  =`Safe: Yor Attendance is higer then 75%`;
-        Status.className = "Safe";
-    } 
-    else {
-        Status.textContent = `Warning : Yor Attendance is Lower then 75%`;
-        Status.className = "Warning";
-    }
-    console.log(Status);
-
-    const PercentageText = document.createElement('div');
-    PercentageText.textContent = `Percentage : ${Percentage}`;
-
-    const RequiredText = document.createElement('div');
-    RequiredText.textContent = `Required Day : ${Requireddays}`;
-
-
-}
-
-
-
 addButton.addEventListener("click",(e)=>{
     e.preventDefault();
 
@@ -59,6 +32,14 @@ addButton.addEventListener("click",(e)=>{
     const buttonContainer = document.createElement('div');
     buttonContainer.id  ="buttonContainer";
 
+    const Percentagetext = document.createElement('div');
+    const Requiredtext = document.createElement('div');
+
+    Percentagetext.className = "Percentagetext";
+    Requiredtext.className = "Requiredtext";
+
+    const Status = document.createElement('div');
+
     buttonContainer.append(presentButton,absentButton);
 
     // presentbuttons event listener
@@ -66,19 +47,17 @@ addButton.addEventListener("click",(e)=>{
         presentClass++;
         totalClass++;
 
-        renderFunction(presentClass,totalClass,PercentageText,RequiredText,card) 
+        renderFunction(presentClass,totalClass,Percentagetext,Requiredtext,Status,card);
     });
 
     // absentButton event listener
     absentButton.addEventListener("click",(e)=>{
         totalClass++;
 
-        renderFunction(presentClass,totalClass,PercentageText,RequiredText,card)
+        renderFunction(presentClass,totalClass,Percentagetext,Requiredtext,Status,card);
     });
-
     
-
-    subjectCardContainer.append(subjectName,buttonContainer,PercentageText,RequiredText);
+    subjectCardContainer.append(subjectName,buttonContainer,Percentagetext,Requiredtext,Status);
     subjectCardContainer.id  = "subjectCardContainer";
     cardContainer.append(subjectCardContainer);
     cardContainer.id = "cardContainer";
@@ -86,3 +65,22 @@ addButton.addEventListener("click",(e)=>{
     card.append(cardContainer);
 
 });
+
+function renderFunction(presentClass,totalClass,Percentagetext,Requiredtext,Status,card) {
+    const Percentage = Math.round((presentClass/totalClass)*100); 
+
+ const Requireddays = Math.max(0,Math.ceil((0.75*totalClass-presentClass)/0.25));
+
+    if(Percentage>=75){  
+        Status.textContent  =`Safe: Yor Attendance is higer then 75%`;
+        Status.classList = "Safe";
+    } 
+    else {
+        Status.textContent = `Warning : Yor Attendance is Lower then 75%`;
+        Status.classList = "Warning";
+    }
+
+    Percentagetext.textContent = `Percentage : ${Percentage}`;
+    Requiredtext.textContent = `Required Day : ${Requireddays}`;
+    
+}
